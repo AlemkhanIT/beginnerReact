92e9562d8f58b2fcb4d229ce5ce3e7a1f6b130d9
AlemkhanIT <captainkenpachi7@gmail.com>
2023-08-01 19:53:56 +0600

    Users from DB

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,31 @@
+import { useEffect } from 'react';
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Success } from './components/Success';
+import { Users } from './components/Users';
+
+// Тут список пользователей: https://reqres.in/api/users
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [users, setUsers] = useState([]);
+  const [isLoading,setLoading] = useState(true);
+  useEffect(()=>{
+    fetch('https://reqres.in/api/users')
+    .then((res) => res.json())
+    .then((json)=>{
+      setUsers(json.data);
+    })
+    .catch((err)=>{
+      console.warn(err);
+      alert("Error while trying to get information from DB");
+    })
+    .finally(()=>(setLoading(false)));
+  },[]);
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div className="App">
+      <Users items={users} isLoading={isLoading}/>
+      {/* <Success /> */}
+    </div>
   )
 }
 
